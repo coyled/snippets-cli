e1b6854349777174b331c16f987cb8a1d72b2365
Dave Coyle <hello@coyled.com>
2019-07-10 11:21:02 -0700

    change write test to use unique body on each run
    this is prep for adding read tests

--- spec/snippets_spec.rb
+++ spec/snippets_spec.rb
@@ -1,14 +1,20 @@
 $:.unshift File.join(File.dirname(__FILE__), '../lib')
 
+require 'securerandom'
 require 'snippets'
+
+test_id = SecureRandom.hex(8)
+test_user = 'testing'
+test_date = Time.now.strftime('%Y-%m-%d')
+test_body = test_date + ' test number ' + test_id
 
 RSpec.describe Snippets, "#valid" do
     context "with valid user, date, and body" do
         it "store snippet" do
             snippet = Snippets::Entry.new(
-                :user   => 'testing',
-                :date   => '2015-08-01',
-                :body   => 'i did a thing'
+                :user   => test_user,
+                :date   => test_date,
+                :body   => test_body
             )
             snippet.save
         end
